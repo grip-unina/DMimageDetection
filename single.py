52bb8837c76a4a1acb5024558632a8ea1cff22d0
Luis Cosio <luisalfonsocosioizcapa@gmail.com>
2023-11-23 12:39:09 -0600

    Added logic to validate image format, size and compression

--- single.py
+++ single.py
@@ -13,7 +13,7 @@ import os
 import torch
 import psutil
 import numpy as np
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 import torchvision.transforms as transforms
 import networks.networks.resnet_mod as resnet_mod
 
@@ -32,6 +32,35 @@ models_config = {
     },
 }
 
+def compress_and_resize_image(image_path, max_size=(1024, 1024)):
+    """
+    Compresses and resizes an image to a manageable size.
+
+    Args:
+        image_path (str): Path to the image file.
+        max_size (tuple): Maximum width and height of the resized image.
+
+    Returns:
+        str: Path to the processed image.
+    """
+    try:
+        # Validate the file format
+        if not image_path.lower().endswith(('.png', '.jpg', '.jpeg', '.webp')):
+            raise ValueError("Unsupported file format. Accepts only JPEG, PNG, and WebP.")
+
+        # Open and process the image
+        with Image.open(image_path) as img:
+            if img.size[0] > max_size[0] or img.size[1] > max_size[1]:
+                # Resize the image only if it's larger than max_size
+                img.thumbnail(max_size, Image.LANCZOS)
+            # Save the processed image in a lossless format
+            processed_image_path = os.path.splitext(image_path)[0] + "_processed.png"
+            img.save(processed_image_path, format='PNG', optimize=True)
+            return processed_image_path
+
+    except UnidentifiedImageError as exc:
+        # Explicitly re-raising with context from the original exception
+        raise ValueError("Invalid image file or path.") from exc
 
 def print_memory_usage():
     """
@@ -111,7 +140,8 @@ def run_single_test(image_path, debug):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     logits = {}
 
-    img = Image.open(image_path).convert("RGB")
+    processed_image_path = compress_and_resize_image(image_path)
+    img = Image.open(processed_image_path).convert("RGB")
 
     for model_name, config in models_config.items():
         # Load and process each model individually
