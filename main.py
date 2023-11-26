f59a8e1c0152bb51b565305883dace085ff763fa
Luis Cosio <luisalfonsocosioizcapa@gmail.com>
2023-11-26 17:38:30 -0600

    More linting

--- main.py
+++ main.py
@@ -12,7 +12,7 @@
 import os
 import glob
 import argparse
-import torch  # >=1.6.0
+import torch
 import pandas
 import numpy as np
 import tqdm
@@ -29,7 +29,9 @@ def running_tests(data_path, output_dir, weights_dir, csv_file):
 
     print("CURRENT OUT FOLDER")
     print(output_dir)
-    datasets = {os.path.basename(os.path.dirname(_)): _ for _ in glob.glob(DATA_PATH+"*/")}
+    datasets = {
+        os.path.basename(os.path.dirname(_)): _ for _ in glob.glob(DATA_PATH + "*/")
+    }
     csvfilename = csv_file
     outroot = output_dir
 
@@ -41,21 +43,26 @@ def running_tests(data_path, output_dir, weights_dir, csv_file):
     # NOTE: Substitute the device with 'cpu' if gpu acceleration is not required
 
     # device = 'cuda:0'  # in ['cpu', 'cuda:0']
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.is_available():
+        print("Using cuda:0")
+    else:
+        print("Using CPU")
     batch_size = 1
 
     ### list of models
 
     models_list = {
-        'Grag2021_progan': 'Grag2021_progan',
-        'Grag2021_latent': 'Grag2021_latent'
+        "Grag2021_progan": "Grag2021_progan",
+        "Grag2021_latent": "Grag2021_latent",
     }
 
     models_dict = dict()
     transform_dict = dict()
     for model_name in models_list:
-
-        _, model_path, arch, norm_type, patch_size = get_method_here(models_list[model_name], weights_path=weights_dir)
+        _, model_path, arch, norm_type, patch_size = get_method_here(
+            models_list[model_name], weights_path=weights_dir
+        )
 
         model = def_model(arch, model_path, localize=False)
         model = model.to(device).eval()
@@ -64,21 +71,21 @@ def running_tests(data_path, output_dir, weights_dir, csv_file):
 
         if patch_size is not None:
             if isinstance(patch_size, tuple):
-                print('input resize:', patch_size)
+                print("input resize:", patch_size)
                 transform.append(transforms.Resize(*patch_size))
-                transform_key = 'res%d_%s' % (patch_size[0], norm_type)
+                transform_key = "res%d_%s" % (patch_size[0], norm_type)
             else:
                 if patch_size > 0:
-                    print('input crop:', patch_size)
+                    print("input crop:", patch_size)
                     transform.append(CenterCropNoPad(patch_size))
-                    transform_key = 'crop%d_%s' % (patch_size, norm_type)
+                    transform_key = "crop%d_%s" % (patch_size, norm_type)
                 else:
-                    print('input crop pad:', patch_size)
+                    print("input crop pad:", patch_size)
                     transform.append(CenterCropNoPad(-patch_size))
                     transform.append(PaddingWarp(-patch_size))
-                    transform_key = 'cropp%d_%s' % (-patch_size, norm_type)
+                    transform_key = "cropp%d_%s" % (-patch_size, norm_type)
         else:
-            transform_key = 'none_%s' % norm_type
+            transform_key = "none_%s" % norm_type
 
         transform = transform + get_list_norm(norm_type)
         transform = transforms.Compose(transform)
@@ -91,22 +98,26 @@ def running_tests(data_path, output_dir, weights_dir, csv_file):
     ### test
 
     with torch.no_grad():
-        table = pandas.read_csv(csvfilename)[['src', ]]
+        table = pandas.read_csv(csvfilename)[
+            [
+                "src",
+            ]
+        ]
         for dataset in datasets:
             outdir = os.path.join(outroot, dataset)
             if not os.path.exists(outdir):
                 os.makedirs(outdir)
             print(outdir)
-            output_csv = outdir + '/'+dataset+'.csv'
+            output_csv = outdir + "/" + dataset + ".csv"
             rootdataset = DATA_PATH
             if os.path.isfile(output_csv):
-                table_old = table['src']
+                table_old = table["src"]
                 table = pandas.read_csv(output_csv)
-                assert len(table_old) == len(table['src'])
-                assert all([a == b for a, b in zip(table_old, table['src'])])
+                assert len(table_old) == len(table["src"])
+                assert all([a == b for a, b in zip(table_old, table["src"])])
                 del table_old
                 do_models = [_ for _ in models_dict.keys() if _ not in table]
-                print('ok')
+                print("ok")
             else:
                 do_models = list(models_dict.keys())
             do_transforms = set([models_dict[_][0] for _ in do_models])
@@ -118,27 +129,38 @@ def running_tests(data_path, output_dir, weights_dir, csv_file):
 
             batch_img = {k: list() for k in transform_dict}
             batch_id = list()
-            table_to_save = table[table['src'].str.contains(dataset+"/")].copy()
+            table_to_save = table[table["src"].str.contains(dataset + "/")].copy()
             print(dataset, csvfilename, flush=True)
-            print(dataset, 'Number of images:', len(table_to_save))
-            for index, dat in tqdm.tqdm(table_to_save.iterrows(), total=len(table_to_save)):
-                if dataset in dat['src'].split('/')[0]:
-
-                    filename = os.path.join(rootdataset, dat['src'])
+            print(dataset, "Number of images:", len(table_to_save))
+            for index, dat in tqdm.tqdm(
+                table_to_save.iterrows(), total=len(table_to_save)
+            ):
+                if dataset in dat["src"].split("/")[0]:
+                    filename = os.path.join(rootdataset, dat["src"])
                     if not os.path.isfile(filename):
-                        filename = filename[:-4] + '.png'
+                        filename = filename[:-4] + ".png"
                     if not os.path.isfile(filename):
-                        filename = filename[:-4] + '.tif'
+                        filename = filename[:-4] + ".tif"
 
                     for k in transform_dict:
-                        batch_img[k].append(transform_dict[k](Image.open(filename).convert('RGB')))
+                        batch_img[k].append(
+                            transform_dict[k](Image.open(filename).convert("RGB"))
+                        )
                     batch_id.append(index)
 
                     if len(batch_id) >= batch_size:
                         for k in do_transforms:
                             batch_img[k] = torch.stack(batch_img[k], 0)
                         for model_name in do_models:
-                            out_tens = models_dict[model_name][1](batch_img[models_dict[model_name][0]].clone().to(device)).cpu().numpy()
+                            out_tens = (
+                                models_dict[model_name][1](
+                                    batch_img[models_dict[model_name][0]]
+                                    .clone()
+                                    .to(device)
+                                )
+                                .cpu()
+                                .numpy()
+                            )
 
                             if out_tens.shape[1] == 1:
                                 out_tens = out_tens[:, 0]
@@ -161,7 +183,13 @@ def running_tests(data_path, output_dir, weights_dir, csv_file):
                     for k in transform_dict:
                         batch_img[k] = torch.stack(batch_img[k], 0)
                     for model_name in models_dict:
-                        out_tens = models_dict[model_name][1](batch_img[models_dict[model_name][0]].clone().to(device)).cpu().numpy()
+                        out_tens = (
+                            models_dict[model_name][1](
+                                batch_img[models_dict[model_name][0]].clone().to(device)
+                            )
+                            .cpu()
+                            .numpy()
+                        )
 
                         if out_tens.shape[1] == 1:
                             out_tens = out_tens[:, 0]
@@ -178,21 +206,45 @@ def running_tests(data_path, output_dir, weights_dir, csv_file):
                     batch_img = {k: list() for k in transform_dict}
                     batch_id = list()
             if "real" in dataset:
-                table_to_save.insert(1, 'label', False)
+                table_to_save.insert(1, "label", False)
             else:
-                table_to_save.insert(1, 'label', True)
-            table_to_save.to_csv(output_csv, index=False)  # save the results as csv file
+                table_to_save.insert(1, "label", True)
+            table_to_save.to_csv(
+                output_csv, index=False
+            )  # save the results as csv file
 
 
 def main():
     print("Running the Tests")
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", type=str, help="The path to the images of the testset on which the operations have been pre applied with the provided code", default="./TestSetCSV/")
-    parser.add_argument("--out_dir", type=str, help="The Path where the csv containing the outputs of the networks should be saved", default="./results_tst")
-    parser.add_argument("--weights_dir", type=str, help="The path to the weights of the networks", default="./weights")
-    parser.add_argument("--csv_file", type=str, help="The path to the csv file", default="./TestSetCSV/operations.csv")
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        help="The path to the images of the testset on which the operations have been pre applied with the provided code",
+        default="./TestSetCSV/",
+    )
+    parser.add_argument(
+        "--out_dir",
+        type=str,
+        help="The Path where the csv containing the outputs of the networks should be saved",
+        default="./results_tst",
+    )
+    parser.add_argument(
+        "--weights_dir",
+        type=str,
+        help="The path to the weights of the networks",
+        default="./weights",
+    )
+    parser.add_argument(
+        "--csv_file",
+        type=str,
+        help="The path to the csv file",
+        default="./TestSetCSV/operations.csv",
+    )
     args = vars(parser.parse_args())
-    running_tests(args['data_dir'], args['out_dir'], args['weights_dir'], args['csv_file'])
+    running_tests(
+        args["data_dir"], args["out_dir"], args["weights_dir"], args["csv_file"]
+    )
 
 
 main()
--- metrics_evaluation.py
+++ metrics_evaluation.py
@@ -10,58 +10,106 @@
 #
 
 import os
+import argparse
 import numpy as np
 import pandas
 from sklearn import metrics
 import dmetrics
-import argparse
 
 
 def calculate_metrics(csv_path, output_path):
     os.makedirs(output_path, exist_ok=True)
     dist_metrics = {
-        'acc': lambda y_label, y_pred: dmetrics.balanced_accuracy_score(y_label, y_pred > 0),
-        'auc': lambda y_label, y_pred: dmetrics.roc_auc_score(y_label, y_pred),
-        'pd10': lambda y_label, y_pred: dmetrics.pd_at_far(y_label, y_pred, 0.10),
-        'eer': lambda y_label, y_pred: dmetrics.calculate_eer(y_label, y_pred),
-        'pd05': lambda y_label, y_pred: dmetrics.pd_at_far(y_label, y_pred, 0.05),
-        'pd01': lambda y_label, y_pred: dmetrics.pd_at_far(y_label, y_pred, 0.01),
-        'macc': lambda y_label, y_pred: dmetrics.macc(y_label, y_pred),
-        'count1': lambda y_label, y_pred: np.sum(y_label == 1),
-        'count0': lambda y_label, y_pred: np.sum(y_label == 0),
+        "acc": lambda y_label, y_pred: dmetrics.balanced_accuracy_score(
+            y_label, y_pred > 0
+        ),
+        "auc": lambda y_label, y_pred: dmetrics.roc_auc_score(y_label, y_pred),
+        "pd10": lambda y_label, y_pred: dmetrics.pd_at_far(y_label, y_pred, 0.10),
+        "eer": lambda y_label, y_pred: dmetrics.calculate_eer(y_label, y_pred),
+        "pd05": lambda y_label, y_pred: dmetrics.pd_at_far(y_label, y_pred, 0.05),
+        "pd01": lambda y_label, y_pred: dmetrics.pd_at_far(y_label, y_pred, 0.01),
+        "macc": lambda y_label, y_pred: dmetrics.macc(y_label, y_pred),
+        "count1": lambda y_label, y_pred: np.sum(y_label == 1),
+        "count0": lambda y_label, y_pred: np.sum(y_label == 0),
     }
 
-    db0s = ['real_coco_valid', 'real_imagenet_val', 'real_ucid', ]
+    # db0s = ['real_coco_valid', 'real_imagenet_val', 'real_ucid', ]
+    db0s = ["test"]
+
     db1d = {
-        'ProGAN': ['progan_lsun'],
-        'StyleGAN2': ['stylegan2_ffhq_256x256', 'stylegan2_lsundog_256x256', 'stylegan2_afhqv2_512x512', 'stylegan2_ffhq_1024x1024', ],
-        'StyleGAN3': ['stylegan3_r_ffhqu_256x256', 'stylegan3_t_ffhqu_256x256', 'stylegan3_r_afhqv2_512x512', 'stylegan3_r_ffhqu_1024x1024', 'stylegan3_t_afhqv2_512x512', 'stylegan3_t_ffhqu_1024x1024', ],
-        'BigGAN': ['biggan_256', 'biggan_512', ],
-        'EG3d': ['eg3d', ],
-        'Taming Tran.': ['taming-transformers_class2image_ImageNet', 'taming-transformers_noise2image_FFHQ', 'taming-transformers_segm2image_valid'],
-        'DALL·E Mini': ['dalle-mini_valid', ],
-        'DALL·E 2': ['dalle_2', ],
-        'GLIDE': ['glide_text2img_valid', ],
-        'Latent Diff.': ['latent-diffusion_class2image_ImageNet', 'latent-diffusion_noise2image_FFHQ',
-                         'latent-diffusion_noise2image_LSUNbedrooms', 'latent-diffusion_noise2image_LSUNchurches', 'latent-diffusion_text2img_valid'],
-        'Stable Diff.': ['stable_diffusion_256', ],
-        'ADM': ['guided-diffusion_class2image_ImageNet',  'guided-diffusion_noise2image_LSUNbedrooms',
-                'guided-diffusion_noise2image_LSUNcats', 'guided-diffusion_noise2image_LSUNhorses'],
+        "ProGAN": ["progan_lsun"],
+        "StyleGAN2": [
+            "stylegan2_ffhq_256x256",
+            "stylegan2_lsundog_256x256",
+            "stylegan2_afhqv2_512x512",
+            "stylegan2_ffhq_1024x1024",
+        ],
+        "StyleGAN3": [
+            "stylegan3_r_ffhqu_256x256",
+            "stylegan3_t_ffhqu_256x256",
+            "stylegan3_r_afhqv2_512x512",
+            "stylegan3_r_ffhqu_1024x1024",
+            "stylegan3_t_afhqv2_512x512",
+            "stylegan3_t_ffhqu_1024x1024",
+        ],
+        "BigGAN": [
+            "biggan_256",
+            "biggan_512",
+        ],
+        "EG3d": [
+            "eg3d",
+        ],
+        "Taming Tran.": [
+            "taming-transformers_class2image_ImageNet",
+            "taming-transformers_noise2image_FFHQ",
+            "taming-transformers_segm2image_valid",
+        ],
+        "DALL·E Mini": [
+            "dalle-mini_valid",
+        ],
+        # 'DALL·E 2': ['dalle_2', ],
+        "GLIDE": [
+            "glide_text2img_valid",
+        ],
+        "Latent Diff.": [
+            "latent-diffusion_class2image_ImageNet",
+            "latent-diffusion_noise2image_FFHQ",
+            "latent-diffusion_noise2image_LSUNbedrooms",
+            "latent-diffusion_noise2image_LSUNchurches",
+            "latent-diffusion_text2img_valid",
+        ],
+        "Stable Diff.": [
+            "stable_diffusion_256",
+        ],
+        "ADM": [
+            "guided-diffusion_class2image_ImageNet",
+            "guided-diffusion_noise2image_LSUNbedrooms",
+            "guided-diffusion_noise2image_LSUNcats",
+            "guided-diffusion_noise2image_LSUNhorses",
+        ],
     }
 
     # NOTE: all the methodologies to use to evaluate the metrics for
-    mm = ['Grag2021_progan', 'Grag2021_latent']
+    mm = ["Grag2021_progan", "Grag2021_latent"]
     tab_metrics_p1 = pandas.DataFrame(index=db1d.keys(), columns=mm)
     tab_metrics_p2 = pandas.DataFrame(index=db1d.keys(), columns=mm)
 
     tab_rs = []
     for db0 in db0s:
-        tab_rs.append(pandas.read_csv(os.path.join(os.path.join(csv_path, db0), db0 + ".csv"), index_col='src'))
+        tab_rs.append(
+            pandas.read_csv(
+                os.path.join(os.path.join(csv_path, db0), db0 + ".csv"), index_col="src"
+            )
+        )
     tab_r = pandas.concat(tab_rs)
     for db1 in db1d:
         tab_f = []
         for folder in db1d[db1]:
-            tab_f.append(pandas.read_csv(os.path.join(os.path.join(csv_path, folder), folder + ".csv")))
+            tab_f.append(
+                pandas.read_csv(
+                    os.path.join(os.path.join(csv_path, folder), folder + ".csv")
+                )
+            )
         if len(tab_f) > 1:
             tab_f = pandas.concat(tab_f)
         else:
@@ -70,17 +118,17 @@ def calculate_metrics(csv_path, output_path):
         tab_all.append(tab_f)
         tab_all.append(tab_r)
         tab_both = pandas.concat(tab_all)
-        label = tab_both['label']
+        label = tab_both["label"]
         for method in mm:
             predict = tab_both[method]
             v = predict[np.isfinite(predict)]
             predict = predict.clip(np.min(v), np.max(v))
             predict[np.isnan(predict)] = 0.0
-            tab_metrics_p1.loc[db1, method] = dist_metrics['acc'](label, predict)
-            tab_metrics_p2.loc[db1, method] = dist_metrics['auc'](label, predict)
+            tab_metrics_p1.loc[db1, method] = dist_metrics["acc"](label, predict)
+            tab_metrics_p2.loc[db1, method] = dist_metrics["auc"](label, predict)
 
-    tab_metrics_p1.loc['AVR'] = tab_metrics_p1.mean(0)
-    tab_metrics_p2.loc['AVR'] = tab_metrics_p2.mean(0)
+    tab_metrics_p1.loc["AVR"] = tab_metrics_p1.mean(0)
+    tab_metrics_p2.loc["AVR"] = tab_metrics_p2.mean(0)
 
     accuracy_path = os.path.join(output_path, "acc.csv")
     auc_path = os.path.join(output_path, "auc.csv")
@@ -93,10 +141,20 @@ def calculate_metrics(csv_path, output_path):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--csv_dir", type=str, help="The path to of the csv files of the output of the networks", default="./results_tst")
-    parser.add_argument("--out_dir", type=str, help="The Path where the csv containing the calculated metrics of the networks should be saved", default="./")
+    parser.add_argument(
+        "--csv_dir",
+        type=str,
+        help="The path to of the csv files of the output of the networks",
+        default="./results_tst",
+    )
+    parser.add_argument(
+        "--out_dir",
+        type=str,
+        help="The Path where the csv containing the calculated metrics of the networks should be saved",
+        default="./",
+    )
     args = vars(parser.parse_args())
-    calculate_metrics(args['csv_dir'], args['out_dir'])
+    calculate_metrics(args["csv_dir"], args["out_dir"])
 
 
 main()
